c9ff77bb6dc9db0ab494fff2e02ee2f7533c550e
tjtnwhd <tjtnwhd4979@naver.com>
2024-04-17 01:20:09 +0900

    배열reverse예제

--- 3.응용 SW 기초 기술활용/source/day02/src/exam01/ArrayReverse2.java
+++ 3.응용 SW 기초 기술활용/source/day02/src/exam01/ArrayReverse2.java
@@ -3,6 +3,7 @@ package exam01;
 import java.util.Arrays;
 
 public class ArrayReverse2 {
+    //저도 찾아보다가  temp 를 사용해서 만드는 방법도 있다고 해서 올려드려요~
     public static void main(String[] args) {
         int[] array = {20,30,15,99};
         System.out.println("정배치 : "+Arrays.toString(array));
